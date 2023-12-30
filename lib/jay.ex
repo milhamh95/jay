85158c6372b98bac10315a4f8403a9cf45514586
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-12-30 09:28:53 +0700

    feat: initialize project

---
+++ lib/jay.ex
@@ -0,0 +1,18 @@
+defmodule Jay do
+  @moduledoc """
+  Documentation for `Jay`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Jay.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
